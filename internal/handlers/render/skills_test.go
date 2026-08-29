bb1ec52c0a7e7e958451374848901ac530a23388
sabhi128 <sabhiahm12@gmail.com>
2026-08-29 14:08:18 +0500

    chore(render): remove unused assert import from skill tests

--- internal/handlers/render/skills_test.go
+++ internal/handlers/render/skills_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	mockRenderBase "opencsg.com/portal/_mocks/opencsg.com/portal/handlers/render"
 	"opencsg.com/portal/pkg/types"
